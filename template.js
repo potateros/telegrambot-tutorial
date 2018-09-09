cf9a8bd3d14ee6fd0a9a0b332b66925a8d51500a
Eric Ngu <ericngu97@gmail.com>
2018-09-10 02:23:20 +0800

    cleanup

--- template.js
+++
@@ -1,8 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api') //Imports node-telegram-bot-api
-const token = "yeet"//Put your bot token here
-const bot = new TelegramBot(token, {polling: true}) //creates a bot object from TelegramBot from node-telegram-bot-api
-
-// Switches on the bot, then scans each message that gets sent to the bot.
-bot.on('message', (msg) => {
-
-})
